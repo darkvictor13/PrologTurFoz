f91cc3b69910f4edd0cf5c7ff167f67904984600
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2023-04-19 11:07:21 -0300

    primeiro exemplo de entrada

---
+++ input/input1.pl
@@ -0,0 +1,14 @@
+% Fatos de exemplo para teste
+pode_ir(estacao1, estacao2, 10, linha1).
+pode_ir(estacao1, estacao3, 20, linha1).
+pode_ir(estacao2, estacao3, 5, linha2).
+pode_ir(estacao2, estacao4, 15, linha2).
+pode_ir(estacao3, estacao4, 10, linha1).
+distaLR(estacao1, estacao2, 8).
+distaLR(estacao1, estacao3, 16).
+distaLR(estacao1, estacao4, 22).
+distaLR(estacao2, estacao3, 5).
+distaLR(estacao2, estacao4, 15).
+distaLR(estacao3, estacao4, 10).
+estacao_inicial(estacao1).
+estacao_final(estacao4).
