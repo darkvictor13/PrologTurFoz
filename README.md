e9bc9e5331ebfd6405ad69e6348545a730d27059
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2023-04-23 23:40:59 -0300

    por mim foi

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Instruções para execução
+# Prolog Tur Foz
+
+Aplicação para linha de comando escrita em prolog com a finalidade de auxiliar usuários de transporte público em Foz do Iguaçu, a usar o transporte público da cidade.
+
+## Instruções para execução
+
+### Pré-requisitos:
+
+- Aplicação de linha de comando `swipl (swi-prolog)`
+
+### Execução:
+
+A execução da aplicação é feita executando os seguintes comandos:
+
+```bash
+swipl main.pl [arquivo de entrada]
+```
+
+[Arquivo de entrada] é um arquivo prolog contendo o mapa da linha do transporte público e a distância entre duas estações
+
+### Exemplo de execução:
+
+```bash
+swipl main.pl input/input1.pl
+```
+
+### Obtendo menor caminho:
+
+```prolog
+menor_caminho(X).
+```
+
+Tendo em vista que X é uma lista de estações que compõem o menor caminho entre a origem e o destino.
--- main.pl
+++ main.pl
@@ -1,16 +1,23 @@
-% Definir a regra bfs/2 para a busca em largura
+% bfs(+Queue, +Goal, -Path)
+% Queue é uma lista de nós a serem analisados.
+% Goal é o nó objetivo.
+% Path é uma lista de saída que vai se tornando o caminho percorrido.
 bfs([Goal|_], Goal, [Goal]).
 bfs([Node|Queue], Goal, [Node|Path]) :-
+    write('Analisando nó: '),
+    write(Node), nl,
     findall(Child,
         (pode_ir(Node, Child, _, _),
-    	\+ member(Child, [Node|Queue])),
+        \+ member(Child, [Node|Queue])),
         Children),
     append(Queue, Children, NewQueue),
     bfs(NewQueue, Goal, Path).
-    
-% Encontrar o menor caminho utilizando bfs/3
+
+% Predicado para chamar o bfs de maneira mais simples.
+% menor_caminho(-Caminho)
+% Caminho é uma lista de estações que representa o menor caminho entre a estação inicial e a estação final.
 menor_caminho(Caminho) :-
     estacao_inicial(Start),
     estacao_final(Goal),
     bfs([Start], Goal, ReversePath),
-    reverse(ReversePath, Caminho).
+    reverse(Caminho, ReversePath).
